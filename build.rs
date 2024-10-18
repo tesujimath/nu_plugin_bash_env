a809c9740347f8e997f48edfeedddaffbf6f6675
Simon Guest <simon.guest@tesujimath.org>
2024-10-19 12:11:45 +1300

    Fix building in Nix flake

--- build.rs
+++ build.rs
@@ -1,14 +1,74 @@
+use anyhow::anyhow;
 use std::{
     env,
+    fs::{copy, create_dir},
     path::{Path, PathBuf},
 };
 
-const BASH_ENV_JSON_VERSION: &str = "0.6.1";
+// If fetching bash-env-json directly from GitHub, this defines what we fetch.
+// Note that when building the package from the Nix Flake, this is not used.
+// In that case, the package version is as defined in the Flake input.
+const BASH_ENV_JSON_GITHUB_TAG: &str = "0.6.1";
 
-fn fetch_bash_env_json() -> Option<PathBuf> {
-    let out_dir: PathBuf = env::var("OUT_DIR").unwrap().into();
+// install bash-env-json locally if available
+fn install_nix_bash_env_json<P>(out_dir: P) -> Option<PathBuf>
+where
+    P: AsRef<Path>,
+{
+    match env::var("NIX_BASH_ENV_JSON") {
+        Err(_) => {
+            println!("cargo:warning=install_nix_bash_env_json failed to find NIX_BASH_ENV_JSON, falling back to fetch from GitHub");
+            None
+        }
+        Ok(bash_env_json) => {
+            let src_path: PathBuf = bash_env_json.into();
+
+            let bash_env_json_repo_dir = PathBuf::from("bash-env-json");
+            let bash_env_json_repo_path = out_dir.as_ref().join(bash_env_json_repo_dir.as_path());
+
+            match create_dir(&bash_env_json_repo_path) {
+                Err(e) => {
+                    println!(
+                        "cargo:warning=install_nix_bash_env_json failed to create directory {}: {}",
+                        bash_env_json_repo_path.to_string_lossy(),
+                        e
+                    );
+                    None
+                }
+                Ok(_) => {
+                    let dst_path = bash_env_json_repo_path.join("bash-env-json");
+                    match copy(&src_path, &dst_path) {
+                        Err(e) => {
+                            println!(
+                                "cargo:warning=install_nix_bash_env_json failed to copy {} to {}: {}",
+                                src_path.to_string_lossy(),
+                                &dst_path.to_string_lossy(),
+                                e
+                            );
+                            None
+                        }
+                        Ok(_) => {
+                            println!(
+                                "cargo:warning=install_nix_bash_env_json installed {} as {}",
+                                src_path.to_string_lossy(),
+                                &dst_path.to_string_lossy(),
+                            );
+                            Some(dst_path)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// fetch bash-env-json from GitHub
+fn fetch_bash_env_json<P>(out_dir: P) -> Option<PathBuf>
+where
+    P: AsRef<Path>,
+{
     let bash_env_json_repo_dir = PathBuf::from("bash-env-json");
-    let bash_env_json_repo_path = out_dir.join(bash_env_json_repo_dir.as_path());
+    let bash_env_json_repo_path = out_dir.as_ref().join(bash_env_json_repo_dir.as_path());
 
     if Path::exists(&bash_env_json_repo_path) {
         Some(bash_env_json_repo_path)
@@ -18,7 +78,7 @@ fn fetch_bash_env_json() -> Option<PathBuf> {
             "clone",
             "--filter=blob:none",
             "--branch",
-            BASH_ENV_JSON_VERSION,
+            BASH_ENV_JSON_GITHUB_TAG,
             "https://github.com/tesujimath/bash-env-json.git",
             bash_env_json_repo_path_str.as_ref(),
         ];
@@ -45,10 +105,14 @@ fn fetch_bash_env_json() -> Option<PathBuf> {
     }
 }
 
-fn main() -> std::io::Result<()> {
-    if let Some(_bash_env_json_repo_path) = fetch_bash_env_json() {
-        // TODO what now?
-    }
+fn main() -> anyhow::Result<()> {
+    let out_dir: PathBuf = env::var("OUT_DIR").unwrap().into();
 
-    Ok(())
+    match install_nix_bash_env_json(&out_dir) {
+        Some(_path) => Ok(()),
+        None => match fetch_bash_env_json(&out_dir) {
+            Some(_path) => Ok(()),
+            None => Err(anyhow!("failed to fetch bash-env-json from GitHub")),
+        },
+    }
 }
